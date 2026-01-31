03c8920c932e8c539eba1681c5e725c8ef49799c
Crow <86617628+Raven-soul@users.noreply.github.com>
2026-01-31 23:55:32 +0300

    начало работы со с пособностями

--- src/lib/ControllerDB/Repository/AdminRepository.js
+++ src/lib/ControllerDB/Repository/AdminRepository.js
@@ -85,3 +85,74 @@ export const getLastParam = () => {
     `;
     return db.prepare(sql).all();
 };
+
+export const getLastAbilityParam = (param_name = 'type') => {
+    const sql = `
+        with t as (
+            select case when p.name = 'cost' then ab.cost
+                        when p.name = 'cast_time' then ab.casting_time
+                        when p.name = 'range' then ab.range
+                        when p.name = 'type' then ab.type
+                        when p.name = 'recharge' then ab.recharge
+                        when p.name = 'kind' then ab.kind
+                        when p.name = 'hd' then ab.hd_hollow
+                        else ''
+                   end as id,
+            
+                   case when p.name = 'cost' then pcost.name
+                        when p.name = 'cast_time' then pc_time.name
+                        when p.name = 'range' then prange.name
+                        when p.name = 'type' then ptype.name
+                        when p.name = 'recharge' then precharge.name
+                        when p.name = 'kind' then pkind.name
+                        when p.name = 'hd' then phd.name
+                        else ''
+                   end as name,
+                   
+                   case when p.name = 'cost' then pcost.value
+                        when p.name = 'cast_time' then pc_time.value
+                        when p.name = 'range' then prange.value
+                        when p.name = 'type' then ptype.value
+                        when p.name = 'recharge' then precharge.value
+                        when p.name = 'kind' then pkind.value
+                        when p.name = 'hd' then phd.value
+                        else ''
+                   end as val
+                   
+              from (select '${param_name}' as name) p 
+                   left join c_armament_ab ab on true                     
+                   left join c_param ptype on ptype.id = ab.type
+                   left join c_param pcost on pcost.id = ab.cost
+                   left join c_param phd on phd.id = ab.hd_hollow
+                   left join c_param pkind on pkind.id = ab.kind
+                   left join c_param pc_time on pc_time.id = ab.casting_time
+                   left join c_param prange on prange.id = ab.range
+                   left join c_param precharge on precharge.id = ab.recharge
+        ),
+
+        t_all as (
+            select * from t
+            
+             union all
+
+            select -1 as id,
+                   '' as name,
+                   '' as value
+        )
+
+        select *
+          from t_all ta
+         order by ta.id desc
+         limit 1
+    `;
+    return db.prepare(sql).all();
+};
+
+export const getParamListByType = (type_id = 1) => {
+    const sql = `
+        select *
+          from c_param p
+         where p.type = ${type_id}
+    `;
+    return db.prepare(sql).all();
+};
--- src/lib/ControllerDB/Repository/ClassRepository.js
+++ src/lib/ControllerDB/Repository/ClassRepository.js
@@ -2,31 +2,39 @@ import db from './../db_connection';
 
 export const getClassContentData = (class_name = 'Shinigami') => {
     const sql = `
-        select ccd.*
-          from cnt_class_menu class_menu
-               inner join cnt_class class on class.class_id = class_menu.id
-               inner join cnt_class_data ccd on ccd.class_id = class.id
-         where class_menu.latin_name = '${class_name}'
-         order by ccd.id
-      
+        select td.*
+          from c_ticket_menu tm
+               inner join c_ticket_menu_group mg on mg.id = tm.group_id
+               inner join c_ticket_type type on type.id = mg.ticket_type
+                          and type.name = 'class'
+               inner join c_class_ticket ct on ct.class_id = tm.id
+               inner join c_ticket_data td on td.ticket_id = ct.ticket_id
+            
+         where tm.latin_name = '${class_name}'
+         order by td.id      
     `;
     return db.prepare(sql).all();
 };
 
 export const getClassContent = (class_name = 'Shinigami') => {
     const sql = `
-        select class.*
-          from cnt_class_menu class_menu
-               inner join cnt_class class on class.class_id = class_menu.id
-         where class_menu.latin_name = '${class_name}'
+        select rt.*
+          from c_ticket_menu tm
+               inner join c_class_ticket rt on rt.class_id = tm.id
+         where tm.latin_name = '${class_name}'
     `;
     return db.prepare(sql).all();
 };
 
 export const getClassMenuGroupContent = () => {
     const sql = `
-        select *
-          from cnt_class_menu_group ccmg
+        select mg.id,
+               mg.ticket_type,
+               mg.name
+          from c_ticket_menu_group mg
+               inner join c_ticket_type type on type.id = mg.ticket_type
+                          and type.name = 'class'
+         order by mg.id
     `;
     return db.prepare(sql).all();
 };
@@ -34,8 +42,8 @@ export const getClassMenuGroupContent = () => {
 export const getClassMenuContent = (group_id = 1) => {
     const sql = `
         select *
-          from cnt_class_menu ccm
-         where ccm.group_id = ${group_id}
+          from c_ticket_menu tm
+          where tm.group_id = ${group_id}
     `;
     return db.prepare(sql).all();
 };
