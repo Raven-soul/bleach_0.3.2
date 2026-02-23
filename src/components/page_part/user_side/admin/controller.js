ca33c0fd4955b5d5e47d4e96f0ac07bae2225c1a
Crow <86617628+Raven-soul@users.noreply.github.com>
2026-02-23 23:11:22 +0300

    fix

--- src/components/page_part/user_side/admin/controller.js
+++ src/components/page_part/user_side/admin/controller.js
@@ -1,20 +1,20 @@
-'use client';
+// 'use client';
 
-export function ArmamentTable({list}){
-    return (
-        <table className="armament_update">
-            <tr>
-                <th>id</th>
-                <th>ab_name</th>
-            </tr>
-            {list.map((element) => {
-                return(
-                    <tr>
-                        <td>{element.id}</td>
-                        <td>{element.latin_name}</td>
-                    </tr>
-                )
-            })}
-        </table>
-    )
-}
+// export function ArmamentTable({list}){
+//     return (
+//         <table className="armament_update">
+//             <tr>
+//                 <th>id</th>
+//                 <th>ab_name</th>
+//             </tr>
+//             {list.map((element) => {
+//                 return(
+//                     <tr>
+//                         <td>{element.id}</td>
+//                         <td>{element.latin_name}</td>
+//                     </tr>
+//                 )
+//             })}
+//         </table>
+//     )
+// }
