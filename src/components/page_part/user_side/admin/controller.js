cfb59d749bb1ae884238a9f07999ff96dd0bc95b
Crow <86617628+Raven-soul@users.noreply.github.com>
2026-02-26 22:06:34 +0300

    ajax start

--- src/components/page_part/user_side/admin/controller.js
+++ src/components/page_part/user_side/admin/controller.js
@@ -1,20 +1,35 @@
-'use client';
+// 'use client';
+// import { useCallback } from 'react'
 
-export function ArmamentTable({list}){
-    return (
-        <table className="armament_update">
-            <tr>
-                <th>id</th>
-                <th>ab_name</th>
-            </tr>
-            {list.map((element) => {
-                return(
-                    <tr>
-                        <td>{element.id}</td>
-                        <td>{element.latin_name}</td>
-                    </tr>
-                )
-            })}
-        </table>
-    )
-}
+// const handleSubmit = useCallback(() => {
+
+//     fetch('/api/admin/ability', {
+//       method: 'GET',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: { a : 10},
+//     }).then((res) => {
+//       alert('true' + res)
+//     })
+//   }, [])
+
+// export function ArmamentTable({list}){
+//     return (
+//         <div>
+//             <button onClick={handleSubmit} id="button">Data</button>
+//             <table className="armament_update">
+//                 <tr>
+//                     <th>id</th>
+//                     <th>ab_name</th>
+//                 </tr>
+//                 {list.map((element) => {
+//                     return(
+//                         <tr>
+//                             <td>{element.id}</td>
+//                             <td>{element.latin_name}</td>
+//                         </tr>
+//                     )
+//                 })}
+//             </table>
+//         </div>
+//     )
+// }
