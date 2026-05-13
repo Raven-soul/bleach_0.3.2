1369907f80e8b25ba554ae8ac817f7c905f8bdd5
Crow <86617628+Raven-soul@users.noreply.github.com>
2026-05-13 22:40:16 +0300

    fix

--- src/app/grid/armament_abilities/[slug]/page.js
+++ src/app/grid/armament_abilities/[slug]/page.js
@@ -2,3 +2,13 @@ export function generateStaticParams() {
     const pages = ['Shinigami', 'Quincy', 'Arrankar', 'Fullbringer', 'Bount'];
     return pages.map((page) => ({ slug: page }));
 }
+
+export default async function Page({ params }) {
+    const { slug } = await params
+//-----------------------------------------------------------------
+
+    return (
+        <div className="row-2">            
+        </div>
+    )
+}
